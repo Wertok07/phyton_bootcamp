4f9506f751f602363331d191d372420d7cc14729
wczernoch <wojtek.ok@wp.pl>
2021-11-20 19:29:46 +0100

    Zadanie 4 z dnia 10 zrobione

--- 16112021_Dzien10/zadanie4_domowe.py
+++ 16112021_Dzien10/zadanie4_domowe.py
@@ -1,7 +1,52 @@
+"""
+Napisz funkcję tree wypisującą zawartość wskazanego katalogu w postaci drzewa katalogów i plików.
+Napisz skrypt, który skorzysta z tej funkcji
+Przykład użycia:
+$ python tree.py .
+.
+|-- other
+|    |-- create_emails.py
+|    \-- create_logs.py
+\-- README.md
+Do wypisywania zawartości folderu można użyć funkcji scandir z moduły os
+"""
 import os
 
-items = os.scandir(".")
-for element in items:
-    nazwa = element.name
-    if nazwa.count(".") == 0:
-        print(element.name)
+poziom = 0
+
+
+def wypisywanie_folderow(sciezka):
+    items = os.scandir(sciezka)
+    itemslist = os.listdir(sciezka)
+    for num, element in enumerate(items):
+        if element.is_dir():
+            drukowanie(element.name, sciezka.count("/"))
+            path = sciezka + "/" + element.name
+            wypisywanie_folderow(str(path))
+        else:
+            if num == len(itemslist) - 1:
+                drukowanie(element.name, sciezka.count("/"), True)
+            else:
+                drukowanie(element.name, sciezka.count("/"))
+
+
+def drukowanie(text, poz, item=False):
+    if poz:
+        print("|", end="")
+        for p in range(0, poz):
+            if poz > 1 and p < poz-1:
+                print("    |", end="")
+            else:
+                print("    ", end="")
+        if item:
+            print("\--", text)
+        else:
+            print("|--", text)
+    else:
+        if item:
+            print("\--", text)
+        else:
+            print("|--", text)
+
+
+wypisywanie_folderow(".")
