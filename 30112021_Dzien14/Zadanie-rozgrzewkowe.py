05c42acb6905bbba351173ea43f8bc719d9a75e9
wczernoch <wojtek.ok@wp.pl>
2021-11-30 18:16:19 +0100

    13 początek

---
+++ 30112021_Dzien14/Zadanie-rozgrzewkowe.py
@@ -0,0 +1,29 @@
+"""
+Zadanie rozgrzewkowe
+"""
+
+
+class Car:
+
+    def __init__(self, level):
+        self.level = level
+
+    def drive(self, level):
+        if self.level >= level:
+            self.level -= level
+            print("Pozostało:", self.level)
+        else:
+            print("Nie masz bateri")
+
+    def charge(self):
+        self.level += 100
+
+
+car = Car(100)
+
+car.drive(70)
+car.drive(50)
+car.drive(50)
+car.charge()
+car.drive(50)
+
